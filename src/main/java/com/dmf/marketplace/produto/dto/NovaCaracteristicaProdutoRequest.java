a4a708e93ef21febf206b08223cd3159b3cd163a
Uiratan Cavalcante <uiratan@gmail.com>
2025-03-11 15:22:36 -0300

    refactor: retirando codigo desnecessario

--- src/main/java/com/dmf/marketplace/produto/dto/NovaCaracteristicaProdutoRequest.java
+++ src/main/java/com/dmf/marketplace/produto/dto/NovaCaracteristicaProdutoRequest.java
@@ -23,14 +23,6 @@ public class NovaCaracteristicaProdutoRequest {
         return new CaracteristicaProduto(this.nome, this.descricao);
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
     @Override
     public String toString() {
         return "NovaCaracteristicaProdutoRequest{" +
